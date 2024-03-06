304a8e4675874e8343abdb07648f36d2c83b2e83
Anish <anish.yerubandi@dellteam.com>
2024-03-06 11:11:11 +0530

    "interviews "

--- src/main/java/interfaces_prac/CompareManagerSalary.java
+++ src/main/java/interfaces_prac/CompareManagerSalary.java
@@ -2,4 +2,8 @@ package interfaces_prac;
 
 public interface CompareManagerSalary extends compareSalary {
     void compareSalary(double empSal1, double empSal2);
+
+    default String v(){
+        return "";
+    }
 }
